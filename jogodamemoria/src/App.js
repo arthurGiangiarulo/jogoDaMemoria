e1613f9702be95d2973bb83393af8b056ba9a31e
arthurGiangiarulo <bruninha_kbral@hotmail.com>
2023-05-02 20:28:54 -0300

    estruturação inicial do jogo

--- jogodamemoria/src/App.js
+++ jogodamemoria/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import logo from './assets/relimpiadas.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+              
+      <div id="menu">
+          <div className='logo'>
+          <img src={logo} alt='logo'/>
+        </div>
+      
+      </div>
+
+      <div id="gameScreen">
+        <p>jogo</p>
+      </div>
+    
     </div>
   );
 }
