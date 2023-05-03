9f65c3e1220bedb6c7d140a0768ff8d4b466eba4
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-05-03 13:26:50 -0300

    Tentativa de animação das cartinhas

--- jogodamemoria/src/App.js
+++ jogodamemoria/src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import "./App.css";
 import logo from "./assets/relimpiadas.png";
-import carta from "./assets/carta.png";
+import cartaVerso from "./assets/carta.png";
+import cartaFrente from "./assets/cartaFrente.png";
 import play from "./assets/play.png";
 
 function App() {
@@ -18,9 +19,20 @@ function App() {
       </div>
 
       <div id="gameScreen">
-        <div className="carta">
-          <img src={carta} alt="carta" />
+
+      <div id="cartas" className="">
+        
+        <div id="cartaFrente" className="carta">
+          <img src={cartaFrente} alt="carta" />
+          <p>1</p>
         </div>
+        
+        <div id="cartaVerso" className="carta">
+          <img src={cartaVerso} alt="carta" />
+        </div>
+
+      </div>
+
       </div>
     </div>
   );
