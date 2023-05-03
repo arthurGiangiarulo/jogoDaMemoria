39d9dea374892737083d20beb80f90af036094f9
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-05-03 14:24:51 -0300

    Implementação da animação do card quando clica

--- jogodamemoria/src/App.js
+++ jogodamemoria/src/App.js
@@ -1,13 +1,15 @@
-// import logo from './logo.svg';
+
+import React from "react";
 import "./App.css";
 import logo from "./assets/relimpiadas.png";
-import cartaVerso from "./assets/carta.png";
-import cartaFrente from "./assets/cartaFrente.png";
+import Card from "./card";
 import play from "./assets/play.png";
 
 function App() {
+
   return (
     <div id="app">
+
       <div id="menu">
         <div className="logo">
           <img src={logo} alt="logo" />
@@ -20,18 +22,7 @@ function App() {
 
       <div id="gameScreen">
 
-      <div id="cartas" className="">
-        
-        <div id="cartaFrente" className="carta">
-          <img src={cartaFrente} alt="carta" />
-          <p>1</p>
-        </div>
-        
-        <div id="cartaVerso" className="carta">
-          <img src={cartaVerso} alt="carta" />
-        </div>
-
-      </div>
+      <Card/> 
 
       </div>
     </div>
---
+++ jogodamemoria/src/card/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import cartaVerso from "../assets/carta.png";
+import cartaFrente from "../assets/cartaFrente.png";
+import "./style.css";
+
+export default function Card() {
+    
+    const card = document.getElementById("card");
+    card?.addEventListener("click", function () {
+        card.classList.toggle("flip");
+    });
+
+    return (
+        <div id="card" className="" >
+            <div id="cartaFrente" className="carta">
+                <img src={cartaFrente} alt="carta" />
+                <p>1</p>
+            </div>
+        
+            <div id="cartaVerso" className="carta">
+                <img src={cartaVerso} alt="carta" />
+            </div>
+        </div>
+    );
+    }
