8874a1526cb6e87cfd48a9ab6fecaafa5c1f6f24
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-05-03 23:32:06 -0300

    Ajuste do bug da cartinha quando girava

--- jogodamemoria/src/App.js
+++ jogodamemoria/src/App.js
@@ -1,11 +1,23 @@
 
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import logo from "./assets/relimpiadas.png";
 import Card from "./components/card";
 import play from "./assets/play.png";
 
+
 function App() {
+
+  const [cards, setCards] = useState([]);
+  
+  function renderCards (cards) {
+    if (cards % 2 === 0) {
+      console.log("É par");
+    } else {
+      console.log("Não é par");
+    }
+  }
 
   return (
     <div id="app">
@@ -20,8 +32,12 @@ function App() {
         </div>
       </div>
 
+      
       <div id="gameScreen">
-
+      <div>
+        <input placeholder="Qts de cards" onChange={ e => setCards(e.target.value)}/>
+        <button onClick={renderCards()}>Enter</button>
+      </div>
       <Card/> 
 
       </div>
