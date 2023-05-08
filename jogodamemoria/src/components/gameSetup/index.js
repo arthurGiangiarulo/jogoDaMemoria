d6be0a907a13e27f25ecb8c39df8482ea8b3ca49
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2023-05-08 00:18:32 -0300

    gameSetup

--- jogodamemoria/src/components/gameSetup/index.js
+++ jogodamemoria/src/components/gameSetup/index.js
@@ -6,14 +6,14 @@ function GameSetup() {
     return (
         <div id="gameSetup">
             <div id="cards" className="dialogBox">
-                <label className="label">Olá</label>
+                <label className="label">Digite o número de cartas</label>
               <div className="input"> 
-                <input></input>
+                <input type="number" min="4" max="28" maxlength="2" ></input>
                 <button>Enter</button>
               </div>           
             </div>
             <div id="pairs" className="dialogBox">
-                <label className="label">Olá</label>
+                <label className="label">Digite o nome da dupla</label>
                 <div className="input"> 
                 <input></input>
                 <button>Enter</button>
